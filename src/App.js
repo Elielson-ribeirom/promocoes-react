a0f39df64f092cdcdd4d07aa61c21a11968cf149
ELIELSON RIBEIRO MACEDO <elielson.macedo@idp.edu.br>
2021-10-21 16:42:29 -0300

    novas funções

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import './App.css';
+import React from 'react';
+import PromotionCard from 'components/Promotion/Card/Card';
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App" style={{ maxWidth: 800, margin: '30px auto' }}>
+      <PromotionCard promotion={promotion} />
+    </div>
+  );
 };
 
 export default App;
